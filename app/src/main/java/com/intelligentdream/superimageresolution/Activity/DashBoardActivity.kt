72b13979376df21dfd9eb6fde0a4cce26c2f3098
Arun sudharsan <arun.m.sudharsan@gmail.com>
2019-02-14 18:53:26 +0530

    set schema for the image storage and user image

--- app/src/main/java/com/intelligentdream/superimageresolution/Activity/DashBoardActivity.kt
+++ app/src/main/java/com/intelligentdream/superimageresolution/Activity/DashBoardActivity.kt
@@ -1,32 +1,62 @@
 package com.intelligentdream.superimageresolution.Activity
 
 import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.SyncStateContract
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.intelligentdream.superimageresolution.Helpers.IMAGE_PICK_CODE
+import com.intelligentdream.superimageresolution.Helpers.PERMISSION_CODE
+import com.intelligentdream.superimageresolution.Model.Image
 import com.intelligentdream.superimageresolution.R
 import kotlinx.android.synthetic.main.activity_dash_board.*
-import java.util.jar.Manifest
-import com.intelligentdream.superimageresolution.Helpers.*
+
 
 class DashBoardActivity : AppCompatActivity() {
 
+    var mAuth: FirebaseAuth? = null
+    var mDatabase: DatabaseReference? = null
+    var currentUser: FirebaseUser? = null
+    var mStorageRef: StorageReference? = null
+    var dialog: AlertDialog? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dash_board)
+        setContentView(com.intelligentdream.superimageresolution.R.layout.activity_dash_board)
         supportActionBar!!.title = "Dashboard"
 
+        mAuth = FirebaseAuth.getInstance()
+        currentUser = mAuth?.currentUser
+        mDatabase = FirebaseDatabase.getInstance().reference.child("Users")
+        mStorageRef = FirebaseStorage.getInstance().reference.child("Images")
 
         btn_choose_pic.setOnClickListener {
             pickImageFromGalley()
         }
+
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Uploading Image")
+        builder.setMessage("Please Wait Your Image is being Uploaded.")
+
+        dialog = builder.create()
 
 
     }
@@ -62,20 +92,99 @@ class DashBoardActivity : AppCompatActivity() {
                     //granted
                     pickImageFromGalleyUtils()
                 } else {
-                    Toast.makeText(this, "FAILED PLEASE IT REQUIED", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show()
                 }
             }
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            Toast.makeText(this, data?.data.toString(), Toast.LENGTH_LONG).show()
+
+
+            //Toast.makeText(this, data?.data.toString(), Toast.LENGTH_LONG).show()
+            //get current user and add this image under images
+
+            dialog?.show()
+            //pb_loading.visibility = View.VISIBLE
+
+
+            val userId: String = currentUser?.uid.toString()
+            val randomID = mDatabase!!.push().key.toString()
+
+            //first upload original image
+            // and add super image default
+
+            mStorageRef = mStorageRef!!.child(userId).child(randomID)
+
+            var originalImageFilePath = mStorageRef!!.child("originalImage.jpg")
+            var superImageFilePath = mStorageRef!!.child("superImage.jpg")
+            val uri = data?.data
+            if (uri != null) {
+
+
+                var imageUri = Uri.parse(
+                    ContentResolver.SCHEME_ANDROID_RESOURCE +
+                            "://" + resources.getResourcePackageName(com.intelligentdream.superimageresolution.R.drawable.loading)
+                            + '/'.toString() + resources.getResourceTypeName(com.intelligentdream.superimageresolution.R.drawable.loading) +
+                            '/'.toString() + resources.getResourceEntryName(com.intelligentdream.superimageresolution.R.drawable.loading)
+                )
+                superImageFilePath.putFile(imageUri)
+
+                val uploadTask = originalImageFilePath.putFile(uri)
+
+                uploadTask.addOnCompleteListener { task: Task<UploadTask.TaskSnapshot> ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Uploaded the image success", Toast.LENGTH_LONG).show()
+
+                        val downloadOriginalImageUrl =
+                            task.result!!.metadata!!.reference!!.downloadUrl.toString()
+
+
+                        Toast.makeText(this, downloadOriginalImageUrl, Toast.LENGTH_LONG).show()
+                        val LinktoOriginalImage = downloadOriginalImageUrl
+                        val LinktoSuperImage = "link to super image"
+
+                        val Image = Image(randomID, LinktoOriginalImage, LinktoSuperImage)
+
+
+                        mDatabase!!.child(userId).push().setValue(Image)
+                            .addOnCompleteListener { task: Task<Void> ->
+                                if (task.isSuccessful) {
+
+                                    Toast.makeText(this, "updated", Toast.LENGTH_LONG).show()
+                                    // pb_loading.visibility = View.GONE
+
+                                    dialog?.dismiss()
+                                } else {
+
+                                    Toast.makeText(
+                                        this,
+                                        "try again bro ${task.exception.toString().split(":")[0]}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+
+                                    // Snackbar.make(rl_signupActivity, task.exception.toString(), Snackbar.LENGTH_LONG).show()
+
+                                }
+
+                            }
+
+                    }
+                }
+                    .addOnSuccessListener { taskSnapshot: UploadTask.TaskSnapshot? ->
+                        {
+
+                        }
+                    }
+
+            }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.dashboard_menu, menu)
+        menuInflater.inflate(com.intelligentdream.superimageresolution.R.menu.dashboard_menu, menu)
         return true
     }
 
